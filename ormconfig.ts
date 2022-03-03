c2390e33a42538c343f2e2f283074c873cf0b70b
Leonardo Alex Fusinato <leonardo.alex.fusinato@gmail.com>
2022-03-02 23:19:02 -0300

    env fix

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,7 +3,7 @@ module.exports = {
     "type": "postgres",
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "username": process.env.DB_USERNAME,
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSSWORD,
     "database": process.env.DB_NAME,
     "entities": [
