2781e117445d920923b54d3a4fa6027e362467e7
Leonardo Alex Fusinato <leonardo.alex.fusinato@gmail.com>
2022-10-25 22:44:08 -0300

    entities and migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,8 +6,8 @@ module.exports = {
   // username: process.env.DB_USER,
   // password: process.env.DB_PASSSWORD,
   // database: process.env.DB_NAME,
-  entities: ['src/app/models/**/*.ts'],
-  migrations: ['src/database/migrations/**/*.ts'],
+  entities: ['dist/src/app/models/**/*.ts'],
+  migrations: ['dist/src/database/migrations/**/*.ts'],
   cli: {
     migrationsDir: 'src/database/migrations',
   },
