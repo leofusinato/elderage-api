b7d0d7e3d01acd820b01a6709a252a0c5d2d3677
Leonardo Alex Fusinato <leonardo.alex.fusinato@gmail.com>
2022-10-25 21:35:17 -0300

    ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,14 @@
 module.exports = {
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSSWORD,
-  database: process.env.DB_NAME,
-  entities: ["src/app/models/**/*.ts"],
-  migrations: ["src/database/migrations/**/*.ts"],
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSSWORD,
+  // database: process.env.DB_NAME,
+  entities: ['src/app/models/**/*.ts'],
+  migrations: ['src/database/migrations/**/*.ts'],
   cli: {
-    migrationsDir: "src/database/migrations",
+    migrationsDir: 'src/database/migrations',
   },
 };
