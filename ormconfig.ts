c9683f654a2e5ea667ce06b979f5760a7be62f5f
Leonardo Alex Fusinato <leonardo.alex.fusinato@gmail.com>
2022-10-27 23:03:18 -0300

    dynamic enviroment

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,14 +6,28 @@ module.exports = {
   // username: process.env.DB_USER,
   // password: process.env.DB_PASSSWORD,
   // database: process.env.DB_NAME,
-  entities: ['dist/src/app/models/**/*.js'],
-  migrations: ['dist/src/database/migrations/**/*.js'],
+  entities: [
+    process.env.NODE_ENV === 'dev'
+      ? 'src/app/models/**/*.ts'
+      : 'dist/src/app/models/**/*.js',
+  ],
+  migrations: [
+    process.env.NODE_ENV === 'dev'
+      ? 'src/database/migrations/**/*.ts'
+      : 'dist/src/database/migrations/**/*.js',
+  ],
   cli: {
-    migrationsDir: 'dist/src/database/migrations',
+    migrationsDir:
+      process.env.NODE_ENV === 'dev'
+        ? 'src/database/migrations'
+        : 'dist/src/database/migrations',
   },
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  extra:
+    process.env.NODE_ENV === 'prod'
+      ? {
+          ssl: {
+            rejectUnauthorized: true,
+          },
+        }
+      : null,
 };
